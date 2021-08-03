fe5fe3e23f2f4b2a343c97865fb454b62d988c64
nixevienna93 <nomar93imperio@gmail.com>
2021-08-03 10:41:50 +0800

    updated heading text
    checking how to upload

--- src/Tour.js
+++ src/Tour.js
@@ -8,7 +8,7 @@ const Tour = ({id, image, info, price, name, removeTour}) => {
     <img src={image} alt={name} />
     <footer>
       <div className='tour-info'>
-        <h4>{ name }</h4>
+        <h4>{ name } 1231232131</h4>
         <h4 className='tour-price'>${price}</h4>
       </div>
       <p>
