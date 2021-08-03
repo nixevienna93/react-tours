1969620ff1e7af1bdf09bd864265b1f4fa6862ef
nixevienna93 <nomar93imperio@gmail.com>
2021-08-03 10:32:26 +0800

    Updates

---
+++ precache-manifest.d965954583fef710fdc91252f157a2a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8e9d15714083d99a098876bbe7cb0a",
+    "url": "/react-tours/index.html"
+  },
+  {
+    "revision": "d2e0efb6a0ee981af3c0",
+    "url": "/react-tours/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/react-tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e0efb6a0ee981af3c0",
+    "url": "/react-tours/static/js/main.b12ac08b.chunk.js"
+  },
+  {
+    "revision": "3361ebb3c243618fc7ca",
+    "url": "/react-tours/static/js/runtime-main.8e9505e0.js"
+  }
+]);
